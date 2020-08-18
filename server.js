30b24afc8872a07a2bcb9192811dd078da69e87f
RabieLakhdar <lakhdar.rabie.benfaraji@gmail.com>
2020-08-18 18:27:45 +0100

    change server config

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const http = require('http')
 const app = require('./app')
 const mongoose = require('mongoose')
 
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
+const { MONGO_USER, MONGO_PASSWORD, DB_NAME } = process.env;
 
 const server = http.createServer(app)
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-lmzcv.mongodb.net/test?retryWrites=true&w=majority`)
-    .then(() => { server.listen(port); console.log("good running at port "+port) })
+const options = { useUnifiedTopology: true, useNewUrlParser: true };
+const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0-lmzcv.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose
+    .connect(uri, options)
+    .then(() => {
+        server.listen(port);
+        console.log(`🌐 running at port: ${port}`)
+    })
     .catch(err => {
         console.warn(err)
     });
