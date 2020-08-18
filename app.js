cee856145d755e8545bea09e5806d6b591c4b6b7
RabieLakhdar <lakhdar.rabie.benfaraji@gmail.com>
2020-08-18 18:34:59 +0100

    config cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require("cors");
 
 const IndexRouter = require('./api/router/index')
 const UserRouter = require('./api/router/user')
@@ -13,15 +14,23 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //CROS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin,Cache-Control,Accept,X-Access-Token ,X-Requested-With, Content-Type, Access-Control-Request-Method"
+    );
     if (req.method === "OPTIONS") {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json({})
+        return res.status(200).end();
     }
-    next()
-})
+    next();
+});
 
 //Router
 app.use('/', IndexRouter)
